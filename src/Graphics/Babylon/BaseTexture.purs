30a643c26f055fb7d4db019f137c057426b573b8
aratama <aratama@example.com>
2016-11-30 05:28:52 +0900

    fix

--- src/Graphics/Babylon/BaseTexture.purs
+++ src/Graphics/Babylon/BaseTexture.purs
@@ -3,10 +3,6 @@ module Graphics.Babylon.BaseTexture where
 import Control.Monad.Eff (Eff)
 import Data.Unit (Unit)
 import Graphics.Babylon (BABYLON)
-import Graphics.Babylon.Color3 (Color3)
-import Graphics.Babylon.Material (Material)
-import Graphics.Babylon.Types (Scene)
-import Graphics.Babylon.Texture (Texture)
 
 foreign import data BaseTexture :: *
 
