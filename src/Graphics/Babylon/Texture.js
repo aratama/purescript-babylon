01f4e37fd92a42efef8008af725df266ba65a3d6
aratama <aratama@example.com>
2016-11-30 13:46:13 +0900

    fix createTexture

--- src/Graphics/Babylon/Texture.js
+++ src/Graphics/Babylon/Texture.js
@@ -1,10 +1,14 @@
 exports.createTexture = function(path){
     return function(scene){
-        return function(){
-            return new BABYLON.Texture(path, scene, false, true);
+        return function(options){
+            return function(){
+                return new BABYLON.Texture(path, scene, options.noMipmap, options.invertY, options.samplingMode, options.onLoad, options.onError);
+            }
         }
     }
 }
+
+exports.tRILINEAR_SAMPLINGMODE = BABYLON.Texture.TRILINEAR_SAMPLINGMODE;
 
 exports.textureToBaseTexture = function(tex){
     return tex;
--- src/Graphics/Babylon/Texture.purs
+++ src/Graphics/Babylon/Texture.purs
@@ -1,6 +1,8 @@
 module Graphics.Babylon.Texture where
 
+import Control.Alternative (pure)
 import Control.Monad.Eff (Eff)
+import Data.Unit (Unit)
 import Graphics.Babylon (BABYLON)
 import Graphics.Babylon.BaseTexture (BaseTexture)
 import Graphics.Babylon.Types (Scene)
@@ -9,7 +11,28 @@ foreign import data Texture :: *
 
 foreign import data CoordinatesMode :: *
 
-foreign import createTexture :: forall eff. String -> Scene -> Eff (babylon :: BABYLON | eff) Texture
+foreign import createTexture :: forall eff. String -> Scene -> CreateTextureOptions eff -> Eff (babylon :: BABYLON | eff) Texture
+
+type CreateTextureOptions eff = {
+    noMipmap :: Boolean,
+    invertY :: Boolean,
+    samplingMode :: SamplingMode,
+    onLoad :: Unit -> Eff (babylon :: BABYLON | eff) Unit,
+    onError :: Unit -> Eff (babylon :: BABYLON | eff) Unit
+}
+
+defaultCreateTextureOptions :: forall eff. CreateTextureOptions eff
+defaultCreateTextureOptions = {
+    noMipmap: false,
+    invertY: true,
+    samplingMode: tRILINEAR_SAMPLINGMODE,
+    onLoad: pure,
+    onError: pure
+}
+
+foreign import data SamplingMode :: *
+
+foreign import tRILINEAR_SAMPLINGMODE :: SamplingMode
 
 foreign import textureToBaseTexture :: Texture -> BaseTexture
 
