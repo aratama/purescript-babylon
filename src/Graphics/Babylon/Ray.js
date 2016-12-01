402070cfba5649d9e7d07b8820ef598b27790770
aratama <aratama@example.com>
2016-12-02 08:19:32 +0900

    createRayWithLength

--- src/Graphics/Babylon/Ray.js
+++ src/Graphics/Babylon/Ray.js
@@ -1,5 +1,13 @@
 exports.createRay = function(origin){
     return function(direction){
+        return function(){
+            return new BABYLON.Ray(origin, direction);
+        }
+    }
+}
+
+exports.createRayWithLength = function(origin){
+    return function(direction){
         return function(length){
             return function(){
                 return new BABYLON.Ray(origin, direction, length);
--- src/Graphics/Babylon/Ray.purs
+++ src/Graphics/Babylon/Ray.purs
@@ -5,5 +5,7 @@ import Graphics.Babylon (BABYLON)
 import Graphics.Babylon.Vector3 (Vector3)
 import Graphics.Babylon.Types (Ray)
 
-foreign import createRay :: forall eff. Vector3 -> Vector3 -> Number -> Eff (babylon :: BABYLON | eff) Ray
+foreign import createRay :: forall eff. Vector3 -> Vector3 -> Eff (babylon :: BABYLON | eff) Ray
+
+foreign import createRayWithLength :: forall eff. Vector3 -> Vector3 -> Number -> Eff (babylon :: BABYLON | eff) Ray
 
